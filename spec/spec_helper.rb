cbd94ab39737c27bf8e767d46823ba3e28176b91
Daniel Vinciguerra <daniel.vinciguerra@resultadosdigitais.com.br>
2019-04-03 11:52:48 -0300

    Change name and path of rspec_status file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = "spec/examples.txt"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
