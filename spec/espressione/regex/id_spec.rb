23ef4f33f7c265d04db3ab0ed12eebd5c34e6083
Alexandre <alexandre.prates@resultadosdigitais.com.br>
2019-04-12 08:56:20 -0300

    Convert _ID_ constants to (lazy load) method

--- spec/espressione/regex/id_spec.rb
+++ spec/espressione/regex/id_spec.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe Espressione::Regex::ID do
-  describe "UUID" do
+  describe ".uuid" do
     let(:uuid) { "10fb6ea0-36d4-0137-f8ae-78da0c844ee3" }
 
     it "matches valid uuid value" do
-      expect(uuid).to match Espressione::UUID
+      expect(uuid).to match Espressione.uuid
     end
 
     context "when invalid" do
       it "doesnt match nil values" do
-        expect(nil).not_to match Espressione::UUID
+        expect(nil).not_to match Espressione.uuid
       end
 
       it "doesnt match empty format" do
-        expect("").not_to match Espressione::UUID
+        expect("").not_to match Espressione.uuid
       end
 
       it "doesnt match invalid format" do
-        expect(uuid.reverse).not_to match Espressione::UUID
+        expect(uuid.reverse).not_to match Espressione.uuid
       end
     end
   end
