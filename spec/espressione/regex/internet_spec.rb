5b307283043c6698c812f1e5fa2af6e0ef414a57
Alexandre <alexandre.prates@resultadosdigitais.com.br>
2019-04-12 08:56:42 -0300

    Convert _Internet_ constants to (lazy load) method

--- spec/espressione/regex/internet_spec.rb
+++ spec/espressione/regex/internet_spec.rb
@@ -1,47 +1,47 @@
 # frozen_string_literal: true
 
 RSpec.describe Espressione::Regex::Internet do
-  describe "IP" do
+  describe ".ip" do
     let(:ipv4) { Faker::Internet.ip_v4_address }
 
     it "matches valid ipv4 value" do
-      expect(ipv4).to match Espressione::IP
+      expect(ipv4).to match Espressione.ip
     end
   end
 
-  describe "IPV6" do
+  describe ".ipv6" do
     let(:ipv6) { Faker::Internet.ip_v6_address }
 
     it "matches valid ipv6 value" do
-      expect(ipv6).to match Espressione::IPV6
+      expect(ipv6).to match Espressione.ipv6
     end
   end
 
-  describe "URL" do
+  describe ".url" do
     let(:url) {   Faker::Internet.url }
 
     it "matches valid url value" do
-      expect(url).to match Espressione::URL
+      expect(url).to match Espressione.url
     end
   end
 
-  describe "EMAIL" do
+  describe ".email" do
     let(:email) { Faker::Internet.email }
 
     it "matches valid email value" do
-      expect(email).to match Espressione::EMAIL
+      expect(email).to match Espressione.email
     end
   end
 
-  describe "SUBDOMAIN" do
+  describe ".subdomain" do
     let(:subdomain) { Faker::Internet.domain_name }
 
     it "matches valid subdomain value" do
-      expect(subdomain).to match Espressione::SUBDOMAIN
+      expect(subdomain).to match Espressione.subdomain
     end
   end
 
-  describe "HTML_TAG" do
+  describe ".html_tag" do
     let(:tag) do
       [
         '<a class="test" href="#">Link</a>',
@@ -50,7 +50,7 @@ RSpec.describe Espressione::Regex::Internet do
     end
 
     it "matches valid html tag" do
-      expect(tag).to match Espressione::HTML_TAG
+      expect(tag).to match Espressione.html_tag
     end
   end
 end
